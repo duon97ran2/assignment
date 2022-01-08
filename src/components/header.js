e28d660a3e78922f3a509f3c6a441135d687f2d9
duon97ran2 <duongtaph13276@fpt.edu.vn>
2022-01-08 14:16:13 +0700

    Feat: Create Components: -  Header,footer

---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+const Header = {
+  print(){
+    return`<header>
+    <div class="bg-blue-900 py-4">
+      <a href=""><img src="./asset/images/logo.81e2f86a (1).png"  alt="" class="mx-auto  w-40"></a>
+    </div>
+    <div class="bg-orange-600 flex">
+      <ul class="flex">
+        <li><a href="" class="nav-item__link">Trang chủ</a></li>
+        <li><a href="" class="nav-item__link">Tuyển sinh</a></li>
+        <li><a href="" class="nav-item__link">Chương trình đào tạo</a></li>
+        <li><a href="" class="nav-item__link">Tuyển dụng</a></li>
+        <li><a href="" class="nav-item__link">Góc sinh viên</a></li>
+      </ul>
+      <label for="search-bar">
+        <input type="text" class="block my-2 focus:outline-none pl-1 mx-12 w-56 bg-white" name="" id="">
+      </label>
+      <button class="bg-blue-900 border border-white text-white px-3 my-2">Tìm kiếm</button>
+    </div>
+  </header>`;
+  }
+}
+export default Header;
--- src/components/news.js
+++ src/components/news.js
@@ -1,20 +1,19 @@
-const NewsList ={
-  print(){
-    return `<div class=" news-item">
-    <a href="" class="overflow-hidden mb-3 block"><img src="https://picsum.photos/400/300" class="w-full h-full object-cover hover:scale-110 transition duration-500" alt=""></a>
-    <a href=""><h2 class="news-title">Vinh danh 295 sinh viên FPT Polytechnic hoc kỳ Spring 2018</h2></a>
-    <p class="whitespace-normal">Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội dã tổ chức Lễ tôn vinh 'Ong vàng Poly' tuyên dương 295 cá nhân và tâp thể có thành tích xuất såc trong học tập cũng như hoạt động</p>
-  </div>
-  <div class=" news-item">
-    <a href="" class="overflow-hidden mb-3 block"><img src="https://picsum.photos/400/300" class="w-full h-full object-cover hover:scale-110 transition duration-500" alt=""></a>
-    <a href=""><h2 class="news-title">Sinh viên khôi ngành Kinh të học cách 'cham' vào cåm xüc</h2></a>
-    <p class="whitespace-normal">Với su tham gia của chuyên gia trong linh vuc kinh doanh, chương trinh đã thu hút đông đão sinh viên Cao đang FPT Polytechnic tham du. Dãc biệt các ban sinh viên bị lôi cuốn bới</p>
-  </div>
-  <div class=" news-item">
-    <a href="" class="overflow-hidden mb-3 block"><img src="https://picsum.photos/400/300" class="w-full h-full object-cover hover:scale-110 transition duration-500" alt=""></a>
-    <a href=""><h2 class="news-title">Sinh viên FPoly trải nghiệm thực tê tại Mường Thanh Luxury.</h2></a>
-    <p class="whitespace-normal">Với triết lý đào tạo 'Thực hoc- Thực nghiệp', Cao dang FPT Polytechnic Đà Nẵng thường xuyên tổ chức cho sinh viên tham quan các đơn vi doanh nghiệp</p>
-  </div>`;
+import newsData from "../data";
+
+const NewsList = {
+  print() {
+    return `
+    <div class="grid grid-cols-3 gap-6">
+    ${newsData.map((post) => `
+    <div class=" news-item">
+    <a href="" class="overflow-hidden mb-3 block"><img src="${post.img}" class="w-full h-full object-cover hover:scale-110 transition duration-500" alt=""></a>
+    <a href=""><h2 class="news-title">${post.title}</h2></a>
+    <p class="whitespace-normal">${post.desc}</p>
+    </div>
+    `).join("")}
+    </div>
+    `
+    ;
   }
 }
 export default NewsList;
