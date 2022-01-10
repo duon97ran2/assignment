bd9537b31269d9de4ef95b505f886c3b7abc8a9f
duon97ran2 <duongtaph13276@fpt.edu.vn>
2022-01-10 16:00:39 +0700

    feat: update page

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,8 +1,9 @@
+import Footer from "../components/footer";
 import Header from "../components/header";
 
 const NewsPage = {
   print(){
-    return `${Header.print()} <h1>Tuyển sinh</h1>`
+    return `${Header.print()} <h1>Tuyển dụng</h1> ${Footer.print()}`
   }
 }
 export default NewsPage;
