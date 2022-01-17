c6593dbf65b81740b463a46aa25c94cca874c4fa
duon97ran2 <duongtaph13276@fpt.edu.vn>
2022-01-17 15:12:16 +0700

    update 17/1/2022

--- src/components/nav.js
+++ src/components/nav.js
@@ -11,7 +11,7 @@ const Nav = {
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/admin/dashboard" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard</a>
+                <a href="/admin/dashboard" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"  aria-current="page">Dashboard</a>
 
                 <a href="/admin/news" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" >News</a>
 
@@ -132,4 +132,4 @@ const Nav = {
     `
   },
 };
-export default Nav;
+  export default Nav;
